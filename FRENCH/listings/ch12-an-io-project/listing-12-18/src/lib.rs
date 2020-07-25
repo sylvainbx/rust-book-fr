e16f4364c5e9b614c9b358aa2d543a029891a659
Jimskapt <rami.programmer@gmail.com>
2020-07-25 13:19:01 +0200

    📦 Deploy code for ch12-04 in `FRENCH/listings/`.

---
+++ FRENCH/listings/ch12-an-io-project/listing-12-18/src/lib.rs
@@ -0,0 +1,52 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub recherche: String,
+    pub nom_fichier: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("il n'y a pas assez d'arguments");
+        }
+
+        let recherche = args[1].clone();
+        let nom_fichier = args[2].clone();
+
+        Ok(Config { recherche, nom_fichier })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contenu = fs::read_to_string(config.nom_fichier)?;
+
+    Ok(())
+}
+
+// ANCHOR: here
+pub fn rechercher<'a>(recherche: &str, contenu: &'a str) -> Vec<&'a str> {
+    for ligne in contenu.lines() {
+        if ligne.contains(recherche) {
+            // faire quelquechose avec la ligne ici
+        }
+    }
+}
+// ANCHOR_END: here
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn un_resultat() {
+        let recherche = "duct";
+        let contenu = "\
+Rust:
+sécurité, rapidité, productivité.
+Obtenez les trois en même temps.";
+
+        assert_eq!(vec!["sécurité, rapidité, productivité."], rechercher(recherche, contenu));
+    }
+}
