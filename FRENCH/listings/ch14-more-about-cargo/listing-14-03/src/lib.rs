1929d03e758c3f0a60f051b1f82cadea1c74f3d7
Jimskapt <rami.programmer@gmail.com>
2020-07-27 17:56:08 +0200

    📦 Deploy code for ch14-02 in `FRENCH/listings/`.

---
+++ FRENCH/listings/ch14-more-about-cargo/listing-14-03/src/lib.rs
@@ -0,0 +1,36 @@
+// ANCHOR: here
+//! # Art
+//!
+//! Une bibliothèque pour modéliser des concepts artistiques.
+
+pub mod types {
+    /// Les couleurs primaires du modèle RJB.
+    pub enum CouleurPrimaire {
+        Rouge,
+        Jaune,
+        Bleu,
+    }
+
+    /// Les couleurs secondaires du modèle RJB.
+    pub enum CouleurSecondaire {
+        Orange,
+        Vert,
+        Violet,
+    }
+}
+
+pub mod utilitaires {
+    use crate::types::*;
+
+    /// Combine deux couleurs primaires dans les mêmes quantités pour
+    /// créer une couleur secondaire.
+    pub fn mixer(c1: CouleurPrimaire, c2: CouleurPrimaire) -> CouleurSecondaire {
+        // -- partie masquée ici --
+        // ANCHOR_END: here
+        CouleurSecondaire::Orange
+        // ANCHOR: here
+    }
+}
+// ANCHOR_END: here
+
+fn main() {}
---
+++ FRENCH/listings/ch14-more-about-cargo/listing-14-06/src/main.rs
@@ -0,0 +1,13 @@
+// ANCHOR: here
+use art::mixer;
+use art::CouleurPrimaire;
+
+fn main() {
+    // -- partie masquée ici --
+    // ANCHOR_END: here
+    let rouge = CouleurPrimaire::Rouge;
+    let jaune = CouleurPrimaire::Jaune;
+    mixer(rouge, jaune);
+    // ANCHOR: here
+}
+// ANCHOR_END: here
