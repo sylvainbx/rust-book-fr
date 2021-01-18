ef4b3eb883575e31afad290cfa8ddaaea1e5fbe1
Joël J. Présent <36854931+joeljpresent@users.noreply.github.com>
2021-01-18 21:32:18 +0100

    Proofread Chapter 07-03

--- FRENCH/listings/ch07-managing-growing-projects/listing-07-09/src/lib.rs
+++ FRENCH/listings/ch07-managing-growing-projects/listing-07-09/src/lib.rs
@@ -19,11 +19,11 @@ pub fn manger_au_restaurant() {
     let mut repas = cuisines::PetitDejeuner::en_ete("seigle");
     // On change d'avis sur le pain que nous souhaitons
     repas.tartine_grillee = String::from("blé");
-    println!( "Je voudrait une tartine grillée au {}, s'il vous plaît.",
+    println!( "Je voudrais une tartine grillée au {}, s'il vous plaît.",
               repas.tartine_grillee);
 
-    // La prochaine ligne ne va pas se compiler si nous ne la commentons pas;
-    // car nous ne sommes pas autorisé à voir ou modifier le fruit de saison
+    // La prochaine ligne ne va pas se compiler si nous ne la commentons pas,
+    // car nous ne sommes pas autorisés à voir ou modifier le fruit de saison
     // qui accompagne le repas.
 
     // repas.fruit_de_saison = String::from("myrtilles");
