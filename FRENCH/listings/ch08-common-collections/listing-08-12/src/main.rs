9d5f9366483e7f0ce75860af6d7b61596a6b6f6c
Joël Present <jpresent@opensense.fr>
2021-03-16 20:15:55 +0100

    Proofread ch08-02

--- FRENCH/listings/ch08-common-collections/listing-08-12/src/main.rs
+++ FRENCH/listings/ch08-common-collections/listing-08-12/src/main.rs
@@ -4,8 +4,8 @@ fn main() {
 
     let s = donnee.to_string();
 
-    // cette méthode fonctionne aussi directement sur une
-    // chaîne de caractères pure :
+    // cette méthode fonctionne aussi directement sur un
+    // littéral de chaîne de caractères :
     let s = "contenu initial".to_string();
     // ANCHOR_END: here
 }
