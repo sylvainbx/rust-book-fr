21eeff2043fe0b88525483f05d9ece736cd8cbc6
Joël J. Présent <36854931+joeljpresent@users.noreply.github.com>
2021-04-12 15:08:06 +0200

    mar2021 L03-NL15

--- FRENCH/listings/ch03-common-programming-concepts/no-listing-15-invalid-array-access/src/main.rs
+++ FRENCH/listings/ch03-common-programming-concepts/no-listing-15-invalid-array-access/src/main.rs
@@ -1,8 +1,25 @@
+use std::io;
+
 fn main() {
     let a = [1, 2, 3, 4, 5];
-    let indice = 10;
+
+    println!("Veuillez entrer un indice de tableau.");
+
+    let mut indice = String::new();
+
+    io::stdin()
+        .read_line(&mut indice)
+        .expect("Échec de la lecture de l'entrée utilisateur");
+
+    let indice: usize = indice
+        .trim()
+        .parse()
+        .expect("L'indice entré n'est pas un nombre");
 
     let element = a[indice];
 
-    println!("La valeur de l'élément est : {}", element);
+    println!(
+        "La valeur de l'élément d'indice {} est : {}",
+        indice, element
+    );
 }
