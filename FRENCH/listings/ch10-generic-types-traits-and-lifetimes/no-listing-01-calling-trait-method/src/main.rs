da1e79aad1156b1bafb5bf2d27d729bbc325eafd
Jimskapt <rami.programmer@gmail.com>
2021-11-23 18:26:45 +0100

    ♻️ Update released listings.

--- FRENCH/listings/ch10-generic-types-traits-and-lifetimes/no-listing-01-calling-trait-method/src/main.rs
+++ FRENCH/listings/ch10-generic-types-traits-and-lifetimes/no-listing-01-calling-trait-method/src/main.rs
@@ -1,7 +1,6 @@
-use chapter10::{self, Resumable, Tweet};
+use aggregator::{Resumable, Tweet};
 
 fn main() {
-    // ANCHOR: here
     let tweet = Tweet {
         nom_utilisateur: String::from("jean"),
         contenu: String::from("Bien sûr, les amis, comme vous le savez probablement déjà"),
@@ -10,5 +9,4 @@ fn main() {
     };
     
     println!("1 nouveau tweet : {}", tweet.resumer());
-    // ANCHOR_END: here
 }
